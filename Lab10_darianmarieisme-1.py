92f4d9676fa3bcc263ff67f26f1285013b443989
Darian Bruce <darianmariebruce@gmail.com>
2026-03-29 16:42:27 -0400

    created wordanalyzer file

--- Lab10_darianmarieisme-1.py
+++ Lab10_darianmarieisme-1.py
@@ -3,3 +3,12 @@ Darian Marie Bruce
 This program determines the word count from an assortment
 of files
 03/29/2026'''
+
+
+#color codes ansi escape sequences
+
+orange: str = "\033[38;5;208m"
+blue: str = "\033[38;5;27m"
+purple: str = "\033[38;5;129m"
+green: str = "\033[38;5;46m"
+reset: str = "\033[0m"
---
+++ word_analyzer.py
@@ -0,0 +1,12 @@
+'''Word Count Lab 10 Programming Assignment
+Darian Marie Bruce
+This program contains the class that
+determines the word count from an assortment
+of files
+03/29/2026'''
+
+class WordAnalyzer:
+
+    def __init__(self, filepath):
+
+    def process_file(self):
